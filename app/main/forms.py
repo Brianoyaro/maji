4c38d02451c18ba243a49eafb1f2fd9e201e15e8
Brianoyaro <134777190+Brianoyaro@users.noreply.github.com>
2024-04-04 10:14:12 +0300

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,6 +6,7 @@ from app.models import Users
 
 
 class EditProfileForm(FlaskForm):
+    """form for profile editing"""
     username = StringField('Username', validators=[DataRequired()])
     county = StringField('County', validators=[DataRequired()])
     phone_number = StringField('Phone number', validators=[DataRequired()])
@@ -13,6 +14,7 @@ class EditProfileForm(FlaskForm):
     submit = SubmitField('submit')
     
     def validate_phone_number(self, phone_number):
+        """ensures user enters right format for a phone number"""
         for val in phone_number.data:
             if val not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
                 raise ValidationError('Invalid phone number')
@@ -21,26 +23,31 @@ class EditProfileForm(FlaskForm):
 
 
 class PlaceOrderForm(FlaskForm):
+    """form for placing an order"""
     id = StringField('Enter ID to place order', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 class FilterSellersForm(FlaskForm):
+    """form for filtering users using their county when placing an order"""
     county = StringField('Filter using county', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 class CheckOrderForm(FlaskForm):
+    """form that checks an order using its id"""
     id = StringField('Enter Id', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 class DeleteOrdersForm(FlaskForm):
+    """form that deletes an order using its id"""
     id = StringField('Enter ID to delete', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 class MessageForm(FlaskForm):
+    """form that receieves text message"""
     content = TextAreaField("message content", validators=[DataRequired()])
     to = StringField("receiver", validators=[DataRequired()])
     submit = SubmitField('Submit')
