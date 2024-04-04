5d5a9dbdb22661ce4f8b3acab78f9ccd872635ef
Brianoyaro <134777190+Brianoyaro@users.noreply.github.com>
2024-04-04 10:03:45 +0300

    Update error.py

--- app/error/error.py
+++ app/error/error.py
@@ -5,10 +5,12 @@ from app import db
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    """404 error"""
     return render_template("error/404.html"), 404
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
+    """500 error"""
     db.session.rollback()
     return render_template("error/500.html"), 500
